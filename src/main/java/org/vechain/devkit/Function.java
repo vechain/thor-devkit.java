927219ad34ce9de19043ca65bb708e96121b9f5f
laalaguer <laalaguer@gmail.com>
2020-10-25 20:41:08 +0800

    param and return encode decode

--- src/main/java/org/vechain/devkit/Function.java
+++ src/main/java/org/vechain/devkit/Function.java
@@ -1,8 +1,21 @@
 package org.vechain.devkit;
 
 import java.nio.ByteBuffer;
+
+import com.esaulpaugh.headlong.abi.ABIType;
+import com.esaulpaugh.headlong.abi.ArrayType;
+import com.esaulpaugh.headlong.abi.Tuple;
+import com.esaulpaugh.headlong.abi.TupleType;
+import com.esaulpaugh.headlong.abi.UnitType;
+
 import org.vechain.devkit.cry.Utils;
 
+/**
+ * ABI Function related operations.
+ * 1) Initialize a function instance according to the definition.
+ * 2) Encode the parameters into bytes.
+ * 3) Decode the call return value into Map or JSON.
+ */
 public class Function {
 
     private com.esaulpaugh.headlong.abi.Function f;
@@ -16,23 +29,40 @@ public class Function {
         this.f = com.esaulpaugh.headlong.abi.Function.fromJson(jsonString);
     }
 
+    /** Get the selector in bytes. */
     public byte[] selector(){
         return this.f.selector();
     }
 
+    /**
+     * Encode the parameters into valid byte buffer.
+     * @param args
+     * @return
+     */
     private ByteBuffer encode(Object... args) {
         return this.f.encodeCallWithArgs(args);
     }
 
+    /**
+     * Encode the parameters into valid bytes.
+     * @param args
+     * @return
+     */
     public byte[] encodeToBytes(Object... args) {
         ByteBuffer bb = this.encode(args);
-        bb.rewind();
-        int total = bb.remaining();
+        bb.rewind(); // Set buffer "position" to index 0;
+        int total = bb.remaining(); // Get elements counter in the buffer.
         byte[] r = new byte[total];
         bb.get(r);
         return r;
     }
 
+    /**
+     * Encode the parameters into valid hex format, with or without 0x prefix.
+     * @param prefix0x
+     * @param args
+     * @return
+     */
     public String encodeToHex(boolean prefix0x, Object... args) {
         byte[] b = this.encodeToBytes(args);
         String r = Utils.bytesToHex(b);
@@ -41,5 +71,42 @@ public class Function {
         } else {
             return Utils.remove0x(r);
         }
+    }
+
+    public void decodeReturnToJson(byte[] data) {
+        Tuple value = this.f.decodeReturn(data);
+        System.out.println("Tuple:");
+        for (int i = 0; i < value.size(); i ++) {
+            System.out.printf("[%d]%n", i);
+            System.out.println(value.get(i));
+            System.out.println();
+        }
+
+        System.out.println("TupleType:");
+        TupleType guide = this.f.getOutputTypes();
+        for (ABIType<?> e: guide) {
+            System.out.println(e.getCanonicalType());
+            if (e instanceof UnitType) {
+                System.out.println("UnitType Type.");
+            } else {
+                System.out.println("Not UnitType Type.");
+            }
+            if (e instanceof TupleType) {
+                System.out.println("TupleType.");
+            } else {
+                System.out.println("Not TupleType.");
+            }
+            if (e instanceof ArrayType) {
+                System.out.println("ArrayType Type.");
+            } else {
+                System.out.println("Not ArrayType Type.");
+            }
+            System.out.println();
+        }
+    }
+
+    @Override
+    public String toString() {
+        return this.f.toString();
     }
 }
--- src/test/java/org/vechain/devkit/FunctionTest.java
+++ src/test/java/org/vechain/devkit/FunctionTest.java
@@ -6,34 +6,73 @@ import static org.testng.Assert.assertEquals;
 import org.vechain.devkit.cry.Utils;
 
 public class FunctionTest {
-    static String f1 =
-    "{" +
-    "    \"constant\": false," +
-    "    \"inputs\": [" +
-    "        {" +
-    "            \"name\": \"a1\"," +
-    "            \"type\": \"uint256\"" +
-    "        }," +
-    "        {" +
-    "            \"name\": \"a2\"," +
-    "            \"type\": \"string\"" +
-    "        }" +
-    "    ]," +
-    "    \"name\": \"f1\"," +
-    "    \"outputs\": [" +
-    "        {" +
-    "            \"name\": \"r1\"," +
-    "            \"type\": \"address\"" +
-    "        }," +
-    "        {" +
-    "            \"name\": \"r2\"," +
-    "            \"type\": \"bytes\"" +
-    "        }" +
-    "    ]," +
-    "    \"payable\": False," +
-    "    \"stateMutability\": \"nonpayable\"," +
-    "    \"type\": \"function\"" +
-    "}";
+    final static String f1 =
+    "{"
+    + "    \"constant\": false,"
+    + "    \"inputs\": ["
+    + "        {"
+    + "            \"name\": \"a1\","
+    + "            \"type\": \"uint256\""
+    + "        },"
+    + "        {"
+    + "            \"name\": \"a2\","
+    + "            \"type\": \"string\""
+    + "        }"
+    + "    ],"
+    + "    \"name\": \"f1\","
+    + "    \"outputs\": ["
+    + "        {"
+    + "            \"name\": \"r1\","
+    + "            \"type\": \"address\""
+    + "        },"
+    + "        {"
+    + "            \"name\": \"r2\","
+    + "            \"type\": \"bytes\""
+    + "        }"
+    + "    ],"
+    + "    \"payable\": false,"
+    + "    \"stateMutability\": \"nonpayable\","
+    + "    \"type\": \"function\""
+    + "}";
+
+    final static String f2 = 
+    "{"
+    + "    \"inputs\": [],"
+    + "    \"name\": \"nodes\","
+    + "    \"payable\": false,"
+    + "    \"outputs\": ["
+    + "        {"
+    + "            \"components\": ["
+    + "                {"
+    + "                    \"internalType\": \"address\","
+    + "                    \"name\": \"master\","
+    + "                    \"type\": \"address\""
+    + "                },"
+    + "                {"
+    + "                    \"internalType\": \"address\","
+    + "                    \"name\": \"endorsor\","
+    + "                    \"type\": \"address\""
+    + "                },"
+    + "                {"
+    + "                    \"internalType\": \"bytes32\","
+    + "                    \"name\": \"identity\","
+    + "                    \"type\": \"bytes32\""
+    + "                },"
+    + "                {"
+    + "                    \"internalType\": \"bool\","
+    + "                    \"name\": \"active\","
+    + "                    \"type\": \"bool\""
+    + "                }"
+    + "            ],"
+    + "            \"internalType\": \"struct AuthorityUtils.Candidate[]\","
+    + "            \"name\": \"list\","
+    + "            \"type\": \"tuple[]\""
+    + "        }"
+    + "    ],"
+    + "    \"stateMutability\": \"nonpayable\","
+    + "    \"type\": \"function\""
+    + "}";
+
     @Test
     public void encodeTest() {
         Function f = new Function(f1);
@@ -43,11 +82,24 @@ public class FunctionTest {
         );
     }
 
+    @Test
     public void selectorText() {
         Function f = new Function(f1);
         assertEquals(
              Utils.bytesToHex(f.selector()),
              "27fcbb2f"
         );
+    }
+
+    @Test
+    public void decodeReturnV1() {
+        Function f = new Function(f1);
+        f.decodeReturnToJson(Utils.hexToBytes("000000000000000000000000abc000000000000000000000000000000000000100000000000000000000000000000000000000000000000000000000000000400000000000000000000000000000000000000000000000000000000000000003666f6f0000000000000000000000000000000000000000000000000000000000"));
+    }
+
+    @Test
+    public void decodeReturnV2() {
+        Function f_ = new Function(f2);
+        f_.decodeReturnToJson(Utils.hexToBytes("000000000000000000000000000000000000000000000000000000000000002000000000000000000000000000000000000000000000000000000000000000060000000000000000000000006935455ef590eb8746f5230981d09d3552398018000000000000000000000000b5358b034647202d0cd3d1bf615e63e498e0268249984a53f9397370079bba8d95f5c15c743098fb318483e0cb6bbf46ec89ccfb00000000000000000000000000000000000000000000000000000000000000000000000000000000000000005ff66ee3a3ea2aba2857ea8276edb6190d9a1661000000000000000000000000d51666c6b4fed6070a78691f1f3c8e79ad02e3a076f090d383f49d8faab2eb151241528a552f0ae645f460360a7635b8883987a60000000000000000000000000000000000000000000000000000000000000000000000000000000000000000c5a02c1eac7516a9275d86c1cb39a5262b8684a4000000000000000000000000e32499b4143830f2526c79d388ecee530b6357aac635894a50ce5c74c62d238dbe95bd6a0fa076029d913d76b0d0b111c538153f00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000e8fd586e022f825a109848832d7e552132bc332000000000000000000000000224626926a7a12225a60e127cec119c939db4a5cdbf2712e19af00dc4d376728f7cb06cc215c8e7c53b94cb47cefb4a26ada2a6c0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000ea2e8c9d6dcad9e4be4f1c88a3befb8ea742832e0000000000000000000000001a011475baa1d368fa2d8328a1b7a8d848b62c94c68dc811199d40ff7ecd8c8d46454ad9ac5f5cde9bae32f927fec10d82dbdf7800000000000000000000000000000000000000000000000000000000000000000000000000000000000000004977d68df97bb313b23238520580d8d3a59939bf0000000000000000000000007ad1d568b3fe5bad3fc264aca70bc7bcd5e4a6ff83b137cf7e30864b8a4e56453eb1f094b4434685d86895de38ac2edcf5d3f5340000000000000000000000000000000000000000000000000000000000000000"));
     }
 }
